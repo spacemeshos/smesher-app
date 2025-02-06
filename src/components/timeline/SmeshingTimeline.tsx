7e14a388e17480ba9ec05b873ba3b8b05857757a
brusher_ru <brusher.designer@gmail.com>
2025-02-06 11:15:47 -0300

    tweak: unselect item and hide tooltip on clicking outside timeline

--- src/components/timeline/SmeshingTimeline.tsx
+++ src/components/timeline/SmeshingTimeline.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { Timeline, TimelineGroup } from 'vis-timeline';
 
-import { Box, Text, usePrevious } from '@chakra-ui/react';
+import { Box, Text, useOutsideClick, usePrevious } from '@chakra-ui/react';
 
 import useTimelineData from '../../hooks/useTimelineData';
 import { colors } from '../../theme';
@@ -207,6 +207,19 @@ export default function SmeshingTimeline() {
     y: 0,
     x: -1000,
     content: null,
+  });
+
+  useOutsideClick({
+    ref: rootRef,
+    handler: () => {
+      if (chartRef.current) {
+        setTooltip((prevState) => ({
+          ...prevState,
+          x: -1000,
+        }));
+        chartRef.current.setSelection([]);
+      }
+    },
   });
 
   useEffect(() => {
