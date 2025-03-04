38d6eb1a3a87fa527e6a5e4a8362ee659d5cb3e1
brusher_ru <brusher.designer@gmail.com>
2025-03-04 14:06:15 -0300

    fix: fetch smesher-app.versions.json using HTTPS

--- global.ts
+++ global.ts
@@ -1,3 +1,3 @@
 export const VERSIONS_JSON_URL =
-  'http://configs.spacemesh.network/smesher-app.versions.json';
+  'https://configs.spacemesh.network/smesher-app.versions.json';
 export const OFFICIAL_HOSTED_URL = 'https://smesher-beta.spacemesh.network/';
